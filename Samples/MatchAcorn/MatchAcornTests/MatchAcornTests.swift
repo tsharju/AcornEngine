4f1d46ae5ff28839904d1bf9b7bb450fdb0390ef
Teemu Harju <teemu.harju@gmail.com>
2026-06-29 18:04:52 +0300

    Added new sample game called "MatchAcorn"

---
+++ Samples/MatchAcorn/MatchAcornTests/MatchAcornTests.swift
@@ -0,0 +1,19 @@
+//
+//  MatchAcornTests.swift
+//  MatchAcornTests
+//
+//  Created by Teemu Harju on 29.6.2026.
+//
+
+import Testing
+@testable import MatchAcorn
+
+struct MatchAcornTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
